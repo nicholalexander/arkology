d44ab6d5dfdfc75fc5b0ea9990b13085d042d1c3
nichol alexander <nichol.alexander@gmail.com>
2024-03-23 15:29:31 -0400

    add control-c for breaking loop

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ mod simulation_time;
 mod terminal_interface;
 mod terrain;
 
+use crossterm::event::{self, Event, KeyCode};
 use simulation_time::SimulationTime;
 use terminal_interface::TerminalInterface;
 use terrain::TerrainGrid;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut terrain = TerrainGrid::new(10, 10);
     let mut simulation_time = SimulationTime::new();
@@ -14,12 +14,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         update(&mut terrain, &mut simulation_time);
         terminal_interface.render(&terrain, &simulation_time)?;
+
+        if event::poll(std::time::Duration::from_millis(1))? {
+            if let Event::Key(key_event) = event::read()? {
+                if key_event.code == KeyCode::Char('c')
+                    && key_event
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL)
+                {
+                    break;
+                }
+            }
+        }
     }
+
+    Ok(())
 }
 
 fn update(terrain: &mut TerrainGrid, simulation_time: &mut SimulationTime) {
     simulation_time.advance();
     terrain.update_temperature(simulation_time.hour);
 }
-
-
