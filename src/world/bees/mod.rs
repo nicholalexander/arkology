288b9425be4d0bb3d085ad5cc5da80c54c1bcb81
nichol alexander <nichol.alexander@gmail.com>
2024-03-27 12:48:41 -0400

    moving bees

--- src/world/bees/mod.rs
+++ src/world/bees/mod.rs
@@ -15,6 +15,42 @@ impl Bees {
 
         bees
     }
+
+    pub fn move_all_bees(bees: &mut Vec<Bee>) {
+        for bee in bees.iter_mut() {
+            let current_position = bee.get_position();
+
+            //randomly move up or down or left or right by one square unless they are on the edge
+            let mut x = current_position.0;
+            let mut y = current_position.1;
+            let mut rng = rand::thread_rng();
+            let direction = rng.gen_range(0..=3);
+            match direction {
+                0 => {
+                    if x > 0 {
+                        x -= 1;
+                    }
+                }
+                1 => {
+                    if x < 9 {
+                        x += 1;
+                    }
+                }
+                2 => {
+                    if y > 0 {
+                        y -= 1;
+                    }
+                }
+                3 => {
+                    if y < 9 {
+                        y += 1;
+                    }
+                }
+                _ => {}
+            }
+            bee.move_position(x, y);
+        }
+    }
 }
 
 pub use bee::Bee;
